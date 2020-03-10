a8890b1e068ad4c2ed132b72813fd744faa178dc
Tim Louw <timlouw.8@gmail.com>
2020-03-10 08:01:29 +0200

    Got the geolocation from the user and created logout button in the nav menu

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 // import '../shared/shared.dart';
 import '../services/services.dart';
-
+import 'package:geolocator/geolocator.dart';
 
 class ProfileScreen extends StatefulWidget {
   createState() => ProfileScreenState();
@@ -11,20 +11,22 @@ class ProfileScreenState extends State<ProfileScreen> {
   AuthService auth = AuthService();
   DatabaseService db = DatabaseService();
   num plainAvg = 0;
+  Position _currentPosition;
 
   @override
   void initState() {
     super.initState();
-    print('hello');
     db.stats.listen((doc) => {
-      setState(() {
-        print('hellos');
-        print(doc.data.plainCloudTotalMS);
-        print((doc.data.plainCloudTotalMS / doc.data.plainCloudTotalCalls).toString());
-        plainAvg = doc.data.plainCloudTotalMS / doc.data.plainCloudTotalCalls;
-        print(plainAvg.toString());
-      })
-    });
+          setState(() {
+            print('hellos');
+            print(doc.data.plainCloudTotalMS);
+            print((doc.data.plainCloudTotalMS / doc.data.plainCloudTotalCalls)
+                .toString());
+            plainAvg =
+                doc.data.plainCloudTotalMS / doc.data.plainCloudTotalCalls;
+            print(plainAvg.toString());
+          })
+        });
 
     auth.getUser.then(
       (user) {
@@ -38,91 +40,59 @@ class ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      drawer: Drawer(),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          FlatButton.icon(
-            color: Colors.red,
-            padding: EdgeInsets.all(5),
-            icon: Icon(IconData(57563, fontFamily: 'MaterialIcons'), color: Colors.white60, size: 28),
-            onPressed: () async {
-              await auth.logout();
-              Navigator.pushReplacementNamed(context, '/');
-            },
-            label: Expanded(
-              child: Text('Logout', textAlign: TextAlign.center, textScaleFactor: 1.2),
-            ),
+        appBar: AppBar(),
+        drawer: Drawer(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Text('Top'),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: FlatButton(
+                    color: Colors.red,
+                    padding: EdgeInsets.all(5),
+                    onPressed: () async {
+                      Navigator.pop(context);
+                      await auth.logout();
+                      Navigator.pushReplacementNamed(context, '/');
+                    },
+                    child: Text('Logout'),
+                  ),
+                ),
+              ),
+            ],
           ),
-          SizedBox(height: 15),
-          FlatButton.icon(
-            color: Colors.blue,
-            padding: EdgeInsets.all(5),
-            icon: Icon(IconData(57563, fontFamily: 'MaterialIcons'), color: Colors.white60, size: 28),
-            onPressed: () async {
-              await db.callFunction('plainCloud');
-            },
-            label: Expanded(
-              child: Text('Plain Cloud function', textAlign: TextAlign.center, textScaleFactor: 1.2),
-            ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              if (_currentPosition != null)
+                Text(
+                    "LAT: ${_currentPosition.latitude}, LNG: ${_currentPosition.longitude}"),
+              FlatButton(
+                child: Text("Get location"),
+                onPressed: () {
+                  _getCurrentLocation();
+                },
+              ),
+            ],
           ),
-          Text('Average Time = ${plainAvg.toString()}ms', textAlign: TextAlign.center),
-          SizedBox(height: 15),
-          FlatButton.icon(
-            color: Colors.blue,
-            padding: EdgeInsets.all(5),
-            icon: Icon(IconData(57563, fontFamily: 'MaterialIcons'), color: Colors.white60, size: 28),
-            onPressed: () async {
-              await db.callFunction('sqlCloud');
-            },
-            label: Expanded(
-              child: Text('SQL CF - 156.38.151.52', textAlign: TextAlign.center, textScaleFactor: 1.2),
-            ),
-          ),
-          Text('Average Time = ms', textAlign: TextAlign.center),
-          SizedBox(height: 15),
-          FlatButton.icon(
-            color: Colors.blue,
-            padding: EdgeInsets.all(5),
-            icon: Icon(IconData(57563, fontFamily: 'MaterialIcons'), color: Colors.white60, size: 28),
-            onPressed: () async {
-              await db.callFunction('sqlCloudSS');
-            },
-            label: Expanded(
-              child: Text('SQL CF - 160.119.141.249', textAlign: TextAlign.center, textScaleFactor: 1.2),
-            ),
-          ),
-          Text('Average Time = ms', textAlign: TextAlign.center),
-          SizedBox(height: 15),
-          FlatButton.icon(
-            color: Colors.orange,
-            padding: EdgeInsets.all(5),
-            icon: Icon(IconData(57563, fontFamily: 'MaterialIcons'), color: Colors.white60, size: 28),
-            onPressed: () async {
-              await db.equalFirestore();
-            },
-            label: Expanded(
-              child: Text('Firestore(80 docs)', textAlign: TextAlign.center, textScaleFactor: 1.2),
-            ),
-          ),
-          Text('Average Time = ms', textAlign: TextAlign.center),
-          SizedBox(height: 15),
-          FlatButton.icon(
-            color: Colors.orange,
-            padding: EdgeInsets.all(5),
-            icon: Icon(IconData(57563, fontFamily: 'MaterialIcons'), color: Colors.white60, size: 28),
-            onPressed: () async {
-              await db.filledFirestore();
-            },
-            label: Expanded(
-              child: Text('Firestore(1000 docs)', textAlign: TextAlign.center, textScaleFactor: 1.2),
-            ),
-          ),
-          Text('Average Time = ms', textAlign: TextAlign.center),
-        ],
-      )
-    );
+        ));
+  }
+
+  _getCurrentLocation() {
+    final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+
+    geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+        .then((Position position) {
+      setState(() {
+        _currentPosition = position;
+      });
+    }).catchError((e) {
+      print(e);
+    });
   }
 }
